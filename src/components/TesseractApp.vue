cd1c4b64df2c203fbc58ec370a9eb71f81fba4a0
Duc <luuduc34@hotmail.com>
2022-11-04 12:36:23 +0100

    add scan button

--- src/components/TesseractApp.vue
+++ src/components/TesseractApp.vue
@@ -1,9 +1,17 @@
 
 <template>
     <div>
-        <input type="file" @change="onFileChange"><br>
+        <br>
+        <v-row justify="space-around">
+            <input type="file" name="file" @change="onFileChange" accept="image/png, image/jpg">
+
+            <v-btn depressed color="primary" @click="scan" v-if="image != ''">
+                Click for scan
+            </v-btn>
+        </v-row>
+        <br>
         <div class="d-flex flex-column justify-space-between align-center">
-            <v-slider v-model="width" class="align-self-stretch" min="200" max="500" step="1"></v-slider>
+            <v-slider v-model="width" class="align-self-stretch" min="300" max="800" step="1"></v-slider>
             <v-img :width="width" :src="image">
             </v-img>
         </div>
@@ -34,7 +42,7 @@ export default {
         image: '',
         output: '',
         status: 0,
-        width: 300,
+        width: 400,
     }),
     methods: {
         onFileChange(e) {
@@ -50,8 +58,8 @@ export default {
             reader.onload = (e) => {
                 this.image = e.target.result;
 
-                Tesseract.recognize(
-                    this.image, 'eng',
+                /* Tesseract.recognize(
+                    this.image, 'eng+fra',
                     {
                         logger: m => this.status = m.progress * 100
                     }
@@ -59,17 +67,30 @@ export default {
                     .catch(err => {
                         console.error(err);
                     })
-                    .then(
-                        ({
-                            data: { text }
-                        }) => {
-                            console.log(text)
-                            this.output = text
-                        }
-                    )
+                    .then(out => this.output = out.data.text) */
+                /*  .then(
+                     ({
+                         data: { text }
+                     }) => {
+                         console.log(text)
+                         this.output = text
+                     }
+                 ) */
             };
             reader.readAsDataURL(file);
         },
+        scan() {
+            Tesseract.recognize(
+                this.image, 'eng+fra',
+                {
+                    logger: m => this.status = m.progress * 100
+                }
+            )
+                .catch(err => {
+                    console.error(err);
+                })
+                .then(out => this.output = out.data.text)
+        }
     },
 }
 </script>
